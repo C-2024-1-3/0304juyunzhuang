eb1951263df803c1eb83cf27ec5274642ae0e5be
strike587 <strikejyz@163.com>
2024-12-17 20:18:54 +0800

    Add files via upload

--- 5.2.cpp
+++ 5.2.cpp
@@ -1,24 +1,3 @@
-#include <iostream>
-#include"student.h"             
-using namespace std;
-
-void Student::display()         
-{
-	cout << "num£º" << num << endl;
-	cout << "name£º" << name << endl;
-	cout << "sex£º" << sex << endl;
-}
-void Student::set_value()
-{
-	cout << "The age of the student is:";
-	cin >> num;
-	cout << "The name of the student is:";
-	cin >> name;
-	cout << "The sex of the student is:";
-	cin >> sex;
-}
-
-                     
 #include <iostream>             
 #include "student.h"
 int main()
